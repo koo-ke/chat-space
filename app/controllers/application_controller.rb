3df444193f0a35dc53f6e72adbb151b8933c60b4
koo-ke <hope14kou@hotmail.co.jp>
2019-05-27 10:22:18 +0900

    BASIC認証　ユニコーン関連

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :basic_auth
+  before_action :basic_auth, if: :production?
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def production?
+    Rails.env.production?
   end
 
   def basic_auth
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -62,3 +62,6 @@
 
 server '54.64.90.58', user: 'ec2-user', roles: %w{app db web}
 
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
+
